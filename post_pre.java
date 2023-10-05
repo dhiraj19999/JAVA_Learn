8d4d22504c3c8832a5a05849f50cf2bc7d2d163b
dhiraj19999 <yogeshsamse682@gmail.com>
2023-10-05 23:04:00 +0530

    add

---
+++ post_pre.java
@@ -0,0 +1,17 @@
+
+
+
+class post_pre{
+
+public static void main(String args[])
+
+{
+    int c =10;
+    int y=6;
+    System.out.println(c++) ;// Post method  ans will be 10
+    System.out.println(++c) ;// pre method ans will be 11
+    System.out.println(y<<3);// ans will be 2*2*2 three times multiple by 6=48 shift opreator 
+    System.out.println(y>>3);// ans will be 2*2*2 three times divided by 6 = 8/3=2 shift opreator
+
+}
+}
